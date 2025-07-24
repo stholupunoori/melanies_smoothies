83b8010ba8022ec2ae1b844f19331392fe5f357c
Shiva Kumar Goud T <stholupunoori@gmail.com>
2025-07-24 23:32:13 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -46,6 +46,13 @@ smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermel
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
+if ingredients_list:
+  ingredients_string = ''
 
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ''
+    smoothiesfroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+    
 
 
