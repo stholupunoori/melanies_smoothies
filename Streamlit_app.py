250b32b26bb522d9c172ae180cc55bd60360f69f
Shiva Kumar Goud T <stholupunoori@gmail.com>
2025-07-24 23:28:09 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -43,7 +43,8 @@ if ingredients_list:
 # New Section to display smoothie fruit nutrition information
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 
 
